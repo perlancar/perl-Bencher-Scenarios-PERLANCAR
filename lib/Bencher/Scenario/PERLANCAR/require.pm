af6bb8bffdad54b3bf1215cb84994d02bb7655d4
perlancar (@pc-office) <perlancar@gmail.com>
2016-01-21 22:02:07 +0700

    Add notes to PERLANCAR::require scenario

--- lib/Bencher/Scenario/PERLANCAR/require.pm
+++ lib/Bencher/Scenario/PERLANCAR/require.pm
@@ -9,6 +9,22 @@ use warnings;
 
 our $scenario = {
     summary => 'Benchmark require() in a tight loop/subroutine',
+    description => <<'_',
+
+`require()` can be put inside a block (like a subroutine) to delay loading a
+module.
+
+When a module is already loaded, `require()` should be cheap enough: it just
+checks against `%INC` to see if an entry for the module is there. So it should
+just be the cost of a single hash lookup.
+
+However, for very tight loops/subroutines, you can avoid this cost by putting
+the `require()` inside a state variable, which will cause the `require()` to be
+evaluated just once.
+
+Or, you can also put the `require()` statement outside of the block/subroutine.
+
+_
     participants => [
         {
             name=>'baseline_empty_sub',
@@ -17,6 +33,11 @@ our $scenario = {
         {
             name=>'require_in_sub',
             code_template=>'require File::Find',
+        },
+        {
+            name=>'require_in_sub_pm',
+            code_template=>'require "File/Find.pm"',
+            summary => 'There is no effect of using the path form',
         },
         {
             name=>'require_in_state',
